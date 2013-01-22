d4fde0983b046f3f056f8f6f8b721f94a8b28b27
Marc Remolt <marc.remolt@googlemail.com>
2013-01-22 15:47:58 +0100

    Implemented bootstrap based layout

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category < ActiveRecord::Base
   attr_accessible :name
 
+  validates :name, presence: true, uniqueness: true
+
   def to_s
     name || super
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :category
   attr_accessible :body, :title, :category_id
+
+  validates :title, :body, :category, presence: true
 end
